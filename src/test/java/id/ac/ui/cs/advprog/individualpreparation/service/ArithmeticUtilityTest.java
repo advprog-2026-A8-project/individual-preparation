1a15b2c00ba59bae38f301df83bc778e091579c1
DerNiks <derrick@ui.ac.id>
2026-02-12 17:33:47 +0700

    feat: Unit Test for Arithmetic-Divide

--- src/test/java/id/ac/ui/cs/advprog/individualpreparation/service/ArithmeticUtilityTest.java
+++ src/test/java/id/ac/ui/cs/advprog/individualpreparation/service/ArithmeticUtilityTest.java
@@ -12,4 +12,24 @@ class ArithmeticUtilityTest {
         double result = utility.exponent(2, 4);
         assertEquals(16.0, result);
     }
+
+    @Test
+    void testDivideNormalCase() {
+        double result = utility.divide(10.0, 2.0);
+        assertEquals(5.0, result);
+    }
+
+    @Test
+    void testDivideNegativeNumber(){
+        double result = utility.divide(-10.0, 2.0);
+        assertEquals(-5.0, result);
+    }
+
+    @Test
+    void testDivideByZero(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            utility.divide(10.0, 0.0);
+        });
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
 }
